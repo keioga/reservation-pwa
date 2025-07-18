cac8b9d548f30169f50a019618256b232e9b5cf1
KEI <ko0128@gmail.com>
2025-07-18 12:19:21 +0900

    スタイルの設定

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js');
+}
+
 function reloadCSS() {
   const link = document.createElement("link");
   link.rel = "stylesheet";
